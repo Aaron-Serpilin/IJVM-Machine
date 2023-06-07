9d9d1f6e54151ffec2093c0f7e38fdc77ffd1772
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-06-07 12:57:00 +0200

    Correct Data Acquisition
    Header, Pool Size, Pool Data, Text Size, and Text Data are all acquired correctly and can be easily printed at the end of the init_ijvm function

--- src/machine.c
+++ src/machine.c
@@ -43,9 +43,9 @@ int init_ijvm(char *binary_path)
     return 1;
   }
 
-  word_t machine_data_array[3]; //Stores the header, pool origin, and pool size
+  word_t* machine_data_array = malloc(sizeof(int) * 12); //Stores the header, pool origin, and pool size
 
-  fread(machine_data_array, 4, 3, starting_file_pointer);
+  fread(machine_data_array, 4, 3, starting_file_pointer); //I read first 3 chunks of 4 bytes
 
   for (int i = 0; i < 3; i++) {
     
@@ -64,12 +64,9 @@ int init_ijvm(char *binary_path)
 
   ijvm_machine.constant_pool_size = machine_data_array[2];
 
-  word_t* pool_data = calloc(ijvm_machine.constant_pool_size/4, 4); //Allocate 4 bytes of memory the pool size number of elements
+  word_t* pool_data = calloc(ijvm_machine.constant_pool_size/2, 4); //Allocate 4 bytes of memory the pool size number of elements
 
-  fread(pool_data, 4, ijvm_machine.constant_pool_size/8, starting_file_pointer);
-  // fread(pool_data, 4, ijvm_machine.constant_pool_size/4, starting_file_pointer);
-
-  dprintf("The pool size is %d\n", ijvm_machine.constant_pool_size);
+  fread(pool_data, 4, ijvm_machine.constant_pool_size/4, starting_file_pointer); // I read then all the pool data chunks of 4 bytes
 
   for (int i = 0; i < ijvm_machine.constant_pool_size/4; i++) {
 
@@ -77,34 +74,40 @@ int init_ijvm(char *binary_path)
 
   }
 
-  ijvm_machine.constant_pool_data = pool_data;
+  ijvm_machine.constant_pool_data = pool_data; //The data has (pool_size/4) elements, and each element has 4 bytes. Can be printed in %02X
 
-  int* text_size = calloc(4, 4);
+  int* text_size = calloc(4, 1);
+
+  fread(text_size, 4, 2, starting_file_pointer); //I read the 2 chunks of 4 bytes about the text size
+
+  for (int i = 0; i < 2; i++) {
+    text_size[i] = swap_uint32(text_size[i]);
+  }
+
+  ijvm_machine.text_size = text_size[1];
+
+  word_t* text_data = calloc(ijvm_machine.text_size/4, 4);
+
+  fread(text_data, 2, ijvm_machine.text_size, starting_file_pointer);
+
+  for (int i = 0; i < ijvm_machine.text_size/2; i++) {
+    text_data[i] = swap_uint32(text_data[i]);
+  }
+
+  ijvm_machine.text_data = text_data; //It has size bytes and (size/4) elements
+
+  // dprintf("The header is %02X\n", ijvm_machine.header);
+  // dprintf("The pool size is %d\n", ijvm_machine.constant_pool_size);
+
+  // for (int i = 0; i < ijvm_machine.constant_pool_size/4; i++) {
+  //   dprintf("The %d pool data element is %02X\n", i, ijvm_machine.constant_pool_data[i]); 
+  // };
   
-  dprintf("The header is %02X\n", ijvm_machine.header);
-  dprintf("The pool size is %d\n", ijvm_machine.constant_pool_size);
-  dprintf("The first part of the pool data is %02X\n", ijvm_machine.constant_pool_data[0]);
-  dprintf("The second part of the pool data is %02X\n", ijvm_machine.constant_pool_data[1]);
-  dprintf("The third part of the pool data is %02X\n", ijvm_machine.constant_pool_data[2]);
-  // dprintf("The header is %02x\n", machine_data_array[0]);
-  // dprintf("The origin is %02X\n", machine_data_array[1]);
-  // dprintf("The size is %d\n", machine_data_array[2]);
+  // dprintf("The text size is %d\n", ijvm_machine.text_size);
 
-  
-  /* --- Loop --- */
-
-  // int individual_bytes;
-  // int* byte_array = malloc(sizeof(int) * 5);
- 
-  // for (int i = 0; (individual_bytes = getc(starting_file_pointer)) != EOF; i++) {
-  //   dprintf("%02X", individual_bytes);
-
-  //       if (i % 4 == 3) // Guarantees each line is 8 characters/4 bytes
-  //           putc('\n', out); 
-  // }
-
-  // dprintf("\n");
-  //return -1;
+  // for (int i = 0; i < ijvm_machine.text_size/3; i++) {
+  //   dprintf("The %d text data element is %02X\n", i, ijvm_machine.text_data[i]);
+  // };
 
   fclose(starting_file_pointer);
   
